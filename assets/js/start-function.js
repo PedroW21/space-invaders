1a97808befabb5be2ea4adf689e3bf0005d7fab3
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-03-18 15:04:43 -0300

    Player se move para todas direções

--- assets/js/start-function.js
+++ assets/js/start-function.js
@@ -15,17 +15,68 @@ function start() { // Sintaxe do jQuery
 
     var game = {};
 
+    var KEYS = { //valores decimais das teclas, fonte: IBM
+        W: 87,
+        A: 65,
+        S: 83,
+        D: 68,
+        SPACE: 32
+    };
+
+    game.pressKey = [];
+
+    // Verifica se o usuario precionou alguma tecla
+
+    $(document).keydown(function(e){
+        game.pressKey[e.which] = true; //se sim
+    });
+
+    $(document).keyup(function(e){
+        game.pressKey[e.which] = false; // se nao
+    });
+
     // Game Loop
+
     game.timer = setInterval(loop, 30); //Temporizador para execução da função novamente (mili seg)
 
     function loop () {
         moveBackground();
+        movePlayer();
     }
 
     // Fim do loop
 
+    // Move o background
     function moveBackground() {
         moveToLeft = parseInt($(".bg-game").css("background-position"));
         $(".bg-game").css("background-position", moveToLeft-1);
+    }
+
+    // Função para mover o jogador
+
+    function movePlayer() {
+        if(game.pressKey[KEYS.W]) {
+            var toTop = parseInt($(".player").css("top"));
+            $(".player").css("top", toTop-10);
+        }
+
+        if(game.pressKey[KEYS.A]) {
+            var toLeft = parseInt($(".player").css("left"));
+            $(".player").css("left", toLeft-10);
+        }
+
+        if(game.pressKey[KEYS.S]) {
+            var toTop = parseInt($(".player").css("top"));
+            $(".player").css("top", toTop+10);
+        }
+
+        if(game.pressKey[KEYS.D]) {
+            var toLeft = parseInt($(".player").css("left"));
+            $(".player").css("left", toLeft+10);
+        }
+
+        if(game.pressKey[KEYS.SPACE]) {
+
+        }
     }
 }
