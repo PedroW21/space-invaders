b085987d4b1ff33ad99d0b993a1c3b566d855b51
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-03-18 15:46:49 -0300

    Inimigo tipo01 se move

--- assets/js/start-function.js
+++ assets/js/start-function.js
@@ -23,9 +23,17 @@ function start() { // Sintaxe do jQuery
         SPACE: 32
     };
 
-    game.pressKey = [];
+    var speedEnemyType1 = 3;
+    var positionEnemyAxisY1 = parseInt(Math.random * 606);
+    var positionEnemyAxisY2 = parseInt(Math.random * 606);
+    var positionEnemyAxisY3 = parseInt(Math.random * 606);
+    var positionEnemyAxisY4 = parseInt(Math.random * 606);
+    var positionEnemyAxisY5 = parseInt(Math.random * 606);
+
 
     // Verifica se o usuario precionou alguma tecla
+
+    game.pressKey = [];
 
     $(document).keydown(function(e){
         game.pressKey[e.which] = true; //se sim
@@ -42,6 +50,11 @@ function start() { // Sintaxe do jQuery
     function loop () {
         moveBackground();
         movePlayer();
+        moveEnemy1(".enemy");
+        moveEnemy2(".enemy2");
+        moveEnemy3(".enemy3");
+        moveEnemy4(".enemy4");
+        moveEnemy5(".enemy5");
     }
 
     // Fim do loop
@@ -91,4 +104,71 @@ function start() { // Sintaxe do jQuery
 
         }
     }
+
+    // Função move inimigo tipo 1 (Andromalius)
+
+    function moveEnemy1(numberOfTheEnemy) {
+        
+        positionEnemyAxisX = parseInt($(numberOfTheEnemy).css("left"));
+        $(numberOfTheEnemy).css("left",positionEnemyAxisX-speedEnemyType1);
+        $(numberOfTheEnemy).css("top",positionEnemyAxisY1);
+            
+            if (positionEnemyAxisX<=0) {
+            positionEnemyAxisY1 = parseInt(Math.random() * 606);
+            $(numberOfTheEnemy).css("left",694);
+            $(numberOfTheEnemy).css("top",positionEnemyAxisY1);
+        }
+    }   
+
+    function moveEnemy2(numberOfTheEnemy) {
+        
+        positionEnemyAxisX = parseInt($(numberOfTheEnemy).css("left"));
+        $(numberOfTheEnemy).css("left",positionEnemyAxisX-speedEnemyType1);
+        $(numberOfTheEnemy).css("top",positionEnemyAxisY2);
+            
+            if (positionEnemyAxisX<=0) {
+            positionEnemyAxisY2 = parseInt(Math.random() * 606);
+            $(numberOfTheEnemy).css("left",694);
+            $(numberOfTheEnemy).css("top",positionEnemyAxisY2);
+        }
+    }   
+
+    function moveEnemy3(numberOfTheEnemy) {
+        
+        positionEnemyAxisX = parseInt($(numberOfTheEnemy).css("left"));
+        $(numberOfTheEnemy).css("left",positionEnemyAxisX-speedEnemyType1);
+        $(numberOfTheEnemy).css("top",positionEnemyAxisY3);
+            
+            if (positionEnemyAxisX<=0) {
+            positionEnemyAxisY3 = parseInt(Math.random() * 606);
+            $(numberOfTheEnemy).css("left",694);
+            $(numberOfTheEnemy).css("top",positionEnemyAxisY3);
+        }
+    }   
+
+    function moveEnemy4(numberOfTheEnemy) {
+        
+        positionEnemyAxisX = parseInt($(numberOfTheEnemy).css("left"));
+        $(numberOfTheEnemy).css("left",positionEnemyAxisX-speedEnemyType1);
+        $(numberOfTheEnemy).css("top",positionEnemyAxisY4);
+            
+            if (positionEnemyAxisX<=0) {
+            positionEnemyAxisY4 = parseInt(Math.random() * 606);
+            $(numberOfTheEnemy).css("left",694);
+            $(numberOfTheEnemy).css("top",positionEnemyAxisY4);
+        }
+    }   
+
+    function moveEnemy5(numberOfTheEnemy) {
+        
+        positionEnemyAxisX = parseInt($(numberOfTheEnemy).css("left"));
+        $(numberOfTheEnemy).css("left",positionEnemyAxisX-speedEnemyType1);
+        $(numberOfTheEnemy).css("top",positionEnemyAxisY5);
+            
+            if (positionEnemyAxisX<=0) {
+            positionEnemyAxisY5 = parseInt(Math.random() * 606);
+            $(numberOfTheEnemy).css("left",694);
+            $(numberOfTheEnemy).css("top",positionEnemyAxisY5);
+        }
+    }   
 }
